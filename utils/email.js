2ecae5cbf78f2876e3ef0c976ec197e6282ad22b
web-source-dev <muhammadnouman72321@gmail.com>
2025-02-11 01:36:52 +0500

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
-require('dotenv').config(); // Use dotenv to manage environment variables
-
-const sendEmail = (to, subject, html) => {
+require('dotenv').config(); // Load environment variables
+const sendEmail = async (to, subject, html) => {
   const data = {
     sender: { email: process.env.SENDINBLUE_EMAIL },
     to: [{ email: to }],
@@ -9,20 +8,20 @@ const sendEmail = (to, subject, html) => {
     htmlContent: html,
   };
 
-  return new Promise((resolve, reject) => {
-    axios.post('https://api.sendinblue.com/v3/smtp/email', data, {
+  try {
+    const response = await axios.post('https://api.sendinblue.com/v3/smtp/email', data, {
       headers: {
-        'api-key': process.env.SENDINBLUE_API_KEY,
+        'api-key': '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',
         'Content-Type': 'application/json',
       },
-    })
-    .then(response => {
-      resolve(response.data);  // Resolve the promise with the response data on success
-    })
-    .catch(error => {
-      reject(error);  // Reject the promise on error
     });
-  });
+    console.log('response',response);
+
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
 };
 
 module.exports = sendEmail;
+
